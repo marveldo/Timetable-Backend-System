16776fe5dd862d0468a0e5bd8c4ba98c3b859630
utibe solomon <utibesolomon6@gmail.com>
2024-06-18 21:38:42 +0100

    changed settings

--- Examtimetable/settings.py
+++ Examtimetable/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*y_^djnq57f=%c@^n#_i*_!5skcnhtuq*ux%wdy=dk$h=jkowy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
